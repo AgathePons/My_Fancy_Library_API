ebd6254192ba095534bd830fdab04790d2aae788
AgathePons <ponsagathe37@gmail.com>
2025-05-24 12:30:15 +0200

    TU dto WIP

---
+++ src/test/java/com/example/demo/dto/AuthorDtoTest.java
@@ -0,0 +1,46 @@
+package com.example.demo.dto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AuthorDtoTest {
+  @Test
+  @DisplayName("test default constructor")
+  void testDefaultConstructor() {
+    var authorDto = new AuthorDto();
+    assertAll(
+            () -> assertNull(authorDto.getId(), "id"),
+            () -> assertNull(authorDto.getFirstname(), "firstname"),
+            () -> assertNull(authorDto.getLastname(), "lastname")
+    );
+  }
+
+  @Test
+  @DisplayName("test all arguments constructor")
+  void testAllArgsConstructor() {
+    long id = 666;
+    String firstname = "author firstname";
+    String lastname = "author lastname";
+    var authorDto = new AuthorDto(id, firstname, lastname);
+    assertAll(
+            () -> assertEquals(id, authorDto.getId()),
+            () -> assertEquals(firstname, authorDto.getFirstname()),
+            () -> assertEquals(lastname, authorDto.getLastname())
+    );
+  }
+
+  @Test
+  @DisplayName("test builder")
+  void testBuilder() {
+    String firstname = "author firstname";
+    String lastname = "author lastname";
+    var authorDto = AuthorDto.builder().firstname(firstname).lastname(lastname).build();
+    assertAll(
+            () -> assertNull(authorDto.getId(), "id"),
+            () -> assertEquals(firstname, authorDto.getFirstname()),
+            () -> assertEquals(lastname, authorDto.getLastname())
+    );
+  }
+}
